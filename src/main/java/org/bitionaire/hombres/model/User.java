294a299cc3d3188df357db4ec377ba85e2d8598b
bitionaire <johann.boehler@googlemail.com>
2015-11-08 15:01:00 +0100

    added simple post/get test

--- src/main/java/org/bitionaire/hombres/model/User.java
+++ src/main/java/org/bitionaire/hombres/model/User.java
@@ -1,6 +1,7 @@
 package org.bitionaire.hombres.model;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -12,11 +13,12 @@ import org.glassfish.jersey.linking.InjectLink;
 import java.io.Serializable;
 import java.net.URI;
 
-@ToString @EqualsAndHashCode
+@ToString @EqualsAndHashCode(exclude = { "id", "self" })
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @JsonIgnore
     @Getter private final String id;
     @Getter private final String name;
     @Getter private final String email;
@@ -25,16 +27,21 @@ public class User implements Serializable {
             resource = UsersResource.class,
             style = InjectLink.Style.ABSOLUTE,
             method = "get",
-            bindings = @Binding(name = "username", value = "${instance.username}"),
+            bindings = @Binding(name = "id", value = "${instance.id}"),
             rel = "self"
     )
     @Getter private URI self;
 
-    @JsonCreator
-    public User(@JsonProperty("id") final String id,
-                @JsonProperty("name") final String name,
-                @JsonProperty("email") final String email) {
+    public User(final String id, final String name, final String email) {
         this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    @JsonCreator
+    public User(@JsonProperty("name") final String name,
+                @JsonProperty("email") final String email) {
+        this.id = null;
         this.name = name;
         this.email = email;
     }
--- src/main/java/org/bitionaire/hombres/persistence/UserDAO.java
+++ src/main/java/org/bitionaire/hombres/persistence/UserDAO.java
@@ -9,4 +9,7 @@ public interface UserDAO {
 
     @SqlQuery("SELECT id, name, email FROM users WHERE id = :id")
     User find(@Bind("id") final String id);
+
+    @SqlUpdate("INSERT INTO users (id, name, email) VALUES (:id, :name, :email)")
+    void add(@Bind("id") final String id, @Bind("name") final String name, @Bind("email") final String email);
 }
